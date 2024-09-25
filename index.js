c45c4ebd49016fde6343d86444dbe626dc739cc2
belenrg.ti23@utsjr.edu.mx <belenrg.ti23@utsjr.edu.mx>
2024-09-25 15:02:19 -0600

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/",usuariosRutas);
+app.use(session({
+    secret:process.env.SESSION_SECRETO,
+    reserve:true,
+    saveUninitialized:true
+}));
 
 var port = process.env.PORT || 3000;
 
